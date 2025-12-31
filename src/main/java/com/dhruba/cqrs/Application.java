c0deebe8c0ec5661654d5d07ed6f1d409f5a68df
Dhruba Chakraborty <dhrubachakraborty@Mac.bbrouter>
2025-12-31 22:10:34 +0800

    Rename package

--- src/main/java/com/dhruba/cqrs/Application.java
+++ src/main/java/com/dhruba/cqrs/Application.java
@@ -1,12 +1,11 @@
-package com.example.cqrs;
+package com.dhruba.cqrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome to CQRS project!");
+    static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
