3d50fb036a18e15f98d07c4f92f7584bcbd68ed2
Dhruba Chakraborty <dhrubachakraborty@Dhrubas-MacBook-Air.local>
2026-03-10 23:56:44 +0800

    feat(Init) - Restarted working on Wallet service

---
+++ src/main/java/com/dhruba/cqrs/controller/CommonController.java
@@ -0,0 +1,11 @@
+package com.dhruba.cqrs.controller;
+
+import com.dhruba.cqrs.dto.BodyHandlerRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+
+}
